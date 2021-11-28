3a46e561dedab2e2bc062dae319451bfde8c8776
Leticia Campos <34458509+leticiacamposs2@users.noreply.github.com>
2021-11-28 12:13:00 +0000

    challenge two

--- solutions/easy/java-if-else.java
+++ solutions/easy/java-if-else.java
@@ -11,17 +11,17 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String WORD = "Weird";
+        String ans = "";
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        if (N % 2 == 0) {
-           if ((N >=2 && N <= 5) || (N >= 20)) {
-                WORD = "Not Weird";
-            }
+        if(N % 2 == 1 || (N >= 6 && N <= 20)) {
+          ans = "Weird";
+        } else {
+            ans = "Not Weird";
         }
         
-        System.out.println(WORD);
+        System.out.println(ans);
         
         scanner.close();
     }
