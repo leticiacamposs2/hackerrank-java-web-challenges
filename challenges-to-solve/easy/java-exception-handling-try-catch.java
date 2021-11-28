e007b1b3a275986774c0267615ed132855f29d0e
Leticia Campos <34458509+leticiacamposs2@users.noreply.github.com>
2021-11-28 17:10:52 +0000

    try catch exception incomplete

---
+++ challenges-to-solve/easy/java-exception-handling-try-catch.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        try {
+            int N = scan.nextInt();
+            int Y = scan.nextInt();
+            
+            System.out.println(N/Y);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
+        scan.close();
+    }
+}
